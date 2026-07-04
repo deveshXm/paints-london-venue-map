19fd7cc2870002bebf6c87844bd3869f17756f91
Devesh Meena <nothefakedevesh@gmail.com>
2026-07-04 19:00:03 +0530

    Configure Vercel static build

--- build-config.js
+++ build-config.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 
 const token = process.env.MAPBOX_ACCESS_TOKEN;
 
@@ -6,7 +7,21 @@ if (!token) {
   throw new Error("MAPBOX_ACCESS_TOKEN is required");
 }
 
+const publicDir = path.join(__dirname, "public");
+fs.rmSync(publicDir, { recursive: true, force: true });
+fs.mkdirSync(publicDir, { recursive: true });
+
+for (const file of [
+  "index.html",
+  "styles.css",
+  "app.js",
+  "venues-data.js",
+  "london-boroughs.geojson",
+]) {
+  fs.copyFileSync(path.join(__dirname, file), path.join(publicDir, file));
+}
+
 fs.writeFileSync(
-  "mapbox-token.js",
+  path.join(publicDir, "mapbox-token.js"),
   `window.MAPBOX_ACCESS_TOKEN = ${JSON.stringify(token)};\n`
 );
